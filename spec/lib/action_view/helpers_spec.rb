4ecaaf1d1c55bf15497469751f8011f602891469
yasaichi <yasaichi@users.noreply.github.com>
2017-12-10 11:30:25 +0900

    Improve the expectations in test of ActionView::Helpers

--- spec/lib/action_view/helpers_spec.rb
+++ spec/lib/action_view/helpers_spec.rb
@@ -4,17 +4,21 @@ require "rails_helper"
 
 RSpec.describe ActionView::Helpers, type: :helper do
   describe "#tag" do
-    subject { helper.tag("p", data_attribute.to_sym => "value") }
+    subject { helper.tag("p", options) }
 
     # NOTE: The config file exists in spec/dummy/config/initializers.
     context "when `options` includes no keys specified in the configuration" do
+      let(:options) { { data_attribute.to_sym => "value" } }
       let(:data_attribute) { "data-foo" }
-      it { is_expected.to include data_attribute }
+
+      it { is_expected.to eq '<p data-foo="value" />' }
     end
 
     context "when `options` includes keys specified in the configuration" do
+      let(:options) { { data_attribute.to_sym => "value" } }
       let(:data_attribute) { "data-test" }
-      it { is_expected.not_to include data_attribute }
+
+      it { is_expected.to eq "<p />" }
     end
   end
 end
