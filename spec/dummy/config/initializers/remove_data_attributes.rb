afdbeca906bf8bb5c01f604383edd4d644d26add
yasaichi <yasaichi@users.noreply.github.com>
2017-12-17 22:43:50 +0900

    Rewrite the initializer in dummy app according to the template

--- spec/dummy/config/initializers/remove_data_attributes.rb
+++ spec/dummy/config/initializers/remove_data_attributes.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 RemoveDataAttributes.configure do |config|
+  # Configure which data attributes should be removed.
+  # By default, any attributes aren't removed.
   config.data_attributes = ["data-test"]
 end
