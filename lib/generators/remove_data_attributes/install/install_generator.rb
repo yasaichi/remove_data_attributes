f84c4875734371fd4dda1b4648d6d475966cba78
yasaichi <yasaichi@users.noreply.github.com>
2017-12-17 21:53:07 +0900

    Implement a generator which copies an initializer

--- lib/generators/remove_data_attributes/install/install_generator.rb
+++ lib/generators/remove_data_attributes/install/install_generator.rb
@@ -1,3 +1,15 @@
-class RemoveDataAttributes::InstallGenerator < Rails::Generators::NamedBase
-  source_root File.expand_path('../templates', __FILE__)
+# frozen_string_literal: true
+
+require "rails/generators/base"
+
+module RemoveDataAttributes
+  module Generators
+    class InstallGenerator < ::Rails::Generators::Base
+      source_root ::File.expand_path("../templates", __FILE__)
+
+      def copy_initializer
+        copy_file "remove_data_attributes.rb", "config/initializers/remove_data_attributes.rb"
+      end
+    end
+  end
 end
---
+++ lib/generators/remove_data_attributes/install/templates/remove_data_attributes.rb
@@ -0,0 +1,5 @@
+RemoveDataAttributes.configure do |config|
+  # Configure which data attributes should be removed.
+  # By default, any attributes aren't removed.
+  # config.data_attributes = ["data-test"]
+end
