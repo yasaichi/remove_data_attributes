43a6defbc62095273cef6e8c395e10a9a3cd27e8
yasaichi <yasaichi@users.noreply.github.com>
2017-12-25 18:19:45 +0900

    Implement some methods to load a YAML formatted configuration

--- lib/remove_data_attributes/configuration.rb
+++ lib/remove_data_attributes/configuration.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+require "remove_data_attributes/errors"
+require "yaml"
+
 module RemoveDataAttributes
   class << self
     attr_writer :configuration
@@ -14,7 +17,45 @@ module RemoveDataAttributes
   end
 
   class Configuration
+    SUPPORTED_FILE_FORMATS = {
+      "YAML" => [".yml", ".yaml"].freeze
+    }.freeze
+
     attr_writer :data_attributes
+
+    class << self
+      def from_file(filename)
+        loader = loader_for(::File.extname(filename))
+
+        unless loader
+          message = "This file format is not supported."
+          raise ::RemoveDataAttributes::InvalidConfiguration, message
+        end
+
+        ::File.open(filename) { |file| new(loader.load(file)) }
+      end
+
+      def supported_file_extensions
+        @supported_file_extensions ||= SUPPORTED_FILE_FORMATS.values.flatten.sort
+      end
+
+      private
+
+      # :reek:ControlParameter
+      def loader_for(extname)
+        case extname
+        when *SUPPORTED_FILE_FORMATS["YAML"] then ::YAML
+        end
+      end
+    end
+
+    # :reek:ManualDispatch
+    def initialize(attributes = {})
+      attributes.each do |attribute_name, value|
+        method_name = "#{attribute_name}="
+        public_send(method_name, value) if respond_to?(method_name)
+      end
+    end
 
     def data_attributes
       @data_attributes ||= []
---
+++ lib/remove_data_attributes/errors.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module RemoveDataAttributes
+  class Error < ::StandardError
+  end
+
+  class InvalidConfiguration < ::RemoveDataAttributes::Error
+  end
+end
