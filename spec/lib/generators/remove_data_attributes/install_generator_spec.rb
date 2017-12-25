5a5bedf4974f6d218bb8fc8278a6590fb58a4372
yasaichi <yasaichi@users.noreply.github.com>
2017-12-25 17:26:58 +0900

    Generate a YAML formatted configuration

--- spec/lib/generators/remove_data_attributes/install_generator_spec.rb
+++ spec/lib/generators/remove_data_attributes/install_generator_spec.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require "yaml"
 require "rails_helper"
 require "generators/remove_data_attributes/install/install_generator"
 
@@ -15,20 +16,11 @@ RSpec.describe RemoveDataAttributes::Generators::InstallGenerator, type: :genera
       run_generator
     end
 
-    describe "initializer" do
-      subject { file("config/initializers/remove_data_attributes.rb") }
+    describe "the configuration" do
+      subject { YAML.load(File.read(filename)) }
+      let(:filename) { file("config/remove_data_attributes.yml") }
 
-      let(:expected_content) do
-        /\A
-          RemoveDataAttributes\.configure\ do\ \|config\|\n
-          .*
-          \#\ config.data_attributes\ =\ \[.+\]\n
-          .*
-          end
-        \Z/mx
-      end
-
-      it { is_expected.to exist & have_correct_syntax & contain(expected_content) }
+      it { is_expected.to eq("data_attributes" => nil) }
     end
   end
 end
